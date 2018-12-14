a6a5b24cb32702687089b767e792bff460c676a2
Nabil <nabilcoding@gmail.com>
2018-12-14 14:25:06 +0600

    dashboard item template

---
+++ templates/dashboard_item.php
@@ -0,0 +1,26 @@
+<?php
+function printItem($header, $icon, $subheader, $link='#'){
+    echo '<div class="column">
+                <div class="ui raised segment">
+                    <div class="two column stackable padded ui grid">
+                        <div class="six wide column">
+                            <div class="ui icon header">
+                                <i class="ui icon circular '.$icon.' "></i>
+                            </div>
+                        </div>
+                        <div class="nine wide column">
+                            <div class="ui icon header ">
+                                <a class="ui center huge aligned header number">'.$header.'</a>
+                                <div class="">
+                                    <div class="sub header ">'.$subheader.'</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ui bottom attached label">
+                        <a href="'.$link.'">Sea all <i class="icon arrow right"></i></a>
+                    </div>
+                </div>
+            </div>';
+}
